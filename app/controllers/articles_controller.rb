a10518457928353e4676987ea1868cf9ecf90bdf
Pierre Lasante <pierre.lasante@videotron.ca>
2020-04-14 05:52:46 -0700

    Add new create routes for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,27 @@
 class ArticlesController < ApplicationController
+	
+
+
 	def show
 		@article = Article.find(params[:id])
 	end
 
 	def index
 		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		# we have to white list what's coming from internet
+		@article = Article.new(params.require(:article).permit(:title, :description))
+		if @article.save
+			flash[:notice] = "Article was created successfully"
+			redirect_to @article  # this redirect to 'show'
+		else
+			render 'new'
+		end
 	end
 end
