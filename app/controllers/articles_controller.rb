14d9a2477b51d805ed23f46563676513ce44e24e
Pierre Lasante <pierre.lasante@videotron.ca>
2020-04-24 07:31:54 -0700

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 	def create
 		# we have to white list what's coming from internet
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:notice] = "Article was created successfully"
 			redirect_to @article  # this redirect to 'show'
