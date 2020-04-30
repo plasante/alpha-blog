9ae54a31b57454b66bf7e7df7922b8cd1ab869a6
Pierre Lasante <pierre.lasante@videotron.ca>
2020-04-30 06:32:40 -0700

    Add categories and tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+	def setup
+		@blank_name = " "
+	end
+
 	test "get new category form and create category" do 
 		# simulate https://localhost:3000/categories/new
 		get new_category_path
@@ -20,4 +24,17 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 		assert_match "sports", response.body
 	end
 
+	test "invalid category submission results in failure" do 
+		get new_category_path
+		assert_template 'categories/new'
+		
+		assert_no_difference 'Category.count' do 
+			post categories_path, params: {category: {name: @blank_name}}
+		end
+
+		assert_template 'categories/new'
+
+		assert_select 'div.alert-danger'
+		assert_select 'h4.alert-heading'
+	end
 end
