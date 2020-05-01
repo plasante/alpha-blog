b7d015803b9a48bb52e9e178e89077f79a4fc6b1
Pierre Lasante <pierre.lasante@videotron.ca>
2020-05-01 05:06:37 -0700

    Add admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,6 +6,14 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	def setup
 		@category = Category.create(name: "sports")
+		@user = User.create(username: "Pierre", 
+			                email: "pierre@example.com",
+			                password: "password",
+			                admin: true)
+		@notAdmin = User.create(username: "Not Admin", 
+			                    email: "notadmin@example.com",
+			                    password: "password",
+			                    admin: false)
 	end
 
 	test "should get categories index" do 
@@ -14,6 +22,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	end
 
 	test "should get categories new" do 
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_response :success
 	end
@@ -21,5 +30,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 	test "should get categories show" do 
 		get category_path(@category.id)
 		assert_response :success
+	end
+
+	test "should redirect create action whan admin not logged in" do 
+		sign_in_as(@notAdmin, "password")
+		assert_no_difference 'Category.count' do 
+			post categories_path, params: {category: {name: "sports"}}
+		end
+		assert_redirected_to categories_path
 	end
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -4,10 +4,15 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
 	def setup
 		@blank_name = " "
+		@user = User.create(username: "Pierre", 
+			                email: "pierre@example.com",
+			                password: "password",
+			                admin: true)
 	end
 
 	test "get new category form and create category" do 
 		# simulate https://localhost:3000/categories/new
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template 'categories/new'
 		
@@ -25,6 +30,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 	end
 
 	test "invalid category submission results in failure" do 
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template 'categories/new'
 		
